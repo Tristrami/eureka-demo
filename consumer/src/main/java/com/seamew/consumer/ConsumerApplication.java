e3bc8c862e25bc236b4746006a21a8de441485fb
seamew <seamew121@gmail.com>
2024-03-04 22:30:13 +0800

    update:新增一些eureka配置

--- consumer/src/main/java/com/seamew/consumer/ConsumerApplication.java
+++ consumer/src/main/java/com/seamew/consumer/ConsumerApplication.java
@@ -9,7 +9,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class ConsumerApplication {
 
     public static void main(String[] args) {
-        // 服务发现: https://juejin.cn/post/7020312146516934669
+
         SpringApplication.run(ConsumerApplication.class, args);
     }
 }
